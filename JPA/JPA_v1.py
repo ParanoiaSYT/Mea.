676e1f94e4dda1c0ead26cfd384057b945425ea8
ParanoiaSYT <919824548@qq.com>
2020-07-31 09:26:12 +0800

    v1

--- JPA/JPA_v1.py
+++ JPA/JPA_v1.py
@@ -99,3 +99,76 @@ app.setUp(app.init_set).tearDown(app.tear_down).run()
 # HBox(children=(Button(description='Pause', style=ButtonStyle()), Button(description='Interrupt', style=ButtonS…
 # S21_idx: 5f1ceebbbfde9e062c58b2f1
 
+
+# 测量腔频
+    '''腔频'''
+import numpy as np
+import skrf as rf
+import lab
+
+from cqed_toolbox.qulab_app.toolbox_zw import general_funcs_for_app  as gfa
+from cqed_toolbox.qulab_app.toolbox_zw import fit_model  as fm
+
+
+class S21(lab.Application):
+    """
+    Aquire S21 data from VNA.
+
+    return:
+        Frequency, Re(S21), Im(S21)
+    """
+
+    async def work(self):
+        x = self.rc['NA'].get_Frequency()
+        for i in range(self.settings.get('VNA_repeat', 1)):
+            if self.settings.get('Push process', True):
+                self.processToChange(100.0 / self.settings.get('VNA_repeat', 1))
+            y = np.array(self.rc['NA'].get_S())
+
+            yield x, np.real(y), np.imag(y)
+
+            if self.settings.get('Push process', True):
+                self.increaseProcess()
+
+    def pre_save(self, x, re, im):
+        if self.data.rows > 1:
+            x = x[0]
+            re = np.mean(re, axis=0)
+            im = np.mean(im, axis=0)
+        return x, re, im
+
+    @staticmethod
+    def plot(fig, data):
+        x, re, im = data
+        z = re + 1j * im
+        ax = fig.add_subplot(121)
+        ax.plot(x / 1e9, rf.mag_2_db(np.abs(z)))
+        ax.set_xlabel('Frequency [GHz]')
+        ax.set_ylabel('Amplitude [dB]')
+        ax.set_title('S21')
+
+        ax2 = fig.add_subplot(122)
+        ax2.plot(x / 1e9, np.rad2deg(np.angle(z)))
+        ax2.set_xlabel('Frequency [GHz]')
+        ax2.set_ylabel('Angle [Deg]')
+        ax2.set_title('S21')
+
+    def init_set(self):
+        """ instrument initial settings """
+        gfa.VNA_init_set(obj=self)
+
+    def tear_down(self):
+        if self.settings.get('shut_down_all_instruments', True):
+            self.rc['NA'].init({'Output': 'OFF'})
+
+        res = lab.query('VNA.S21')
+        cou = res.count()
+        print('S21_idx: %s' % res[cou - 1].id)
+
+        if self.settings.get('S21_fit', False):
+            idx = res[cou - 1].id
+            res = lab.query(id__in=['%s' % idx])
+            r = res[0]
+            fm.S21_fit(r, method='UCSB')
+#             fm.S21_fit(r, params=globals().get('params', None), method='UCSB', level=3, skep=2)
+S21.save(package='VNA')
